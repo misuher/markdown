18ca7d14b937300b86d06316a5a7df289f92497b
misuher <cm.castillo87@gmail.com>
2015-07-19 01:18:34 +0200

    server offer form at localhost:3000

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import "net/http"
 
 func main() {
-	http.HandleFunc("/", markdown)       //working page
-	http.HandleFunc("/preview", preview) //ajax url
+	http.Handle("/", http.FileServer(http.Dir("./static/"))) //working page
+	http.HandleFunc("/preview", preview)                     //ajax url
 	http.ListenAndServe(":3000", nil)
 }
 
-func markdown(w http.ResponseWriter, r *http.Request) {
-
-}
-
 func preview(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.NotFound(w, r)
+		return
+	}
 
+	field := r.FormValue("textfield")
+	//TODO: markdown parser call
+	w.Write([]byte(field))
 }
